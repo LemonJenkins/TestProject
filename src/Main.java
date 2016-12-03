a0fc23c99cab51b4d756f4699514bb1d1ef5abc1
LemonJenkins <lem0000000000n@gmail.com>
2016-12-03 16:50:19 +0200

    Hello world fixed)

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import static com.Nixsolutions.testSortArray;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         testSortArray();
     }
 }
